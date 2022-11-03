0a051645b659ebf3c33e663079eac239ec2a7a9a
ShahmeerBhola <shahmir.shamim1@gmail.com>
2022-11-03 16:19:20 +0500

    push

---
+++ src/Pages/Home/MainSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../../components/Wrapper'
+
+const MainSection = () => {
+  return (
+    <Wrapper>
+        hello
+    </Wrapper>
+  )
+}
+
+export default MainSection
